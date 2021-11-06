4fec0c41a929a3df608d897401a28ab4c2b9c778
dslyvka <daniilslyvka@gmail.com>
2021-11-06 17:49:51 +0300

    hw 13 is done!

--- src/index.js
+++ src/index.js
@@ -4,31 +4,64 @@ import photoMarkup from './photoMarkup.hbs';
 
 const searchFormRef = document.querySelector('input');
 const galleryRef = document.querySelector('.gallery');
-searchFormRef.addEventListener('input', debounce(onSearch, 500));
-searchFormRef.addEventListener('keydown', e => {
-  if (e.which === 13) e.preventDefault();
-});
+const loadMoreBtnRef = document.querySelector('[data-action="load more"]');
+loadMoreBtnRef.disabled = true;
 
 const queryStringParam = {
   KEY: '24204810-4c4e56177cf5555097dc8a654',
   page: 1,
   LINK: 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=',
+  resetPage() {
+    this.page = 1;
+  },
+  pageIncrement() {
+    this.page += 1;
+  },
 };
+
+searchFormRef.addEventListener('input', debounce(onSearch, 500));
+
+searchFormRef.addEventListener('keydown', e => {
+  if (e.which === 13) e.preventDefault();
+});
+
+loadMoreBtnRef.addEventListener('click', onBtnClick);
 
 function onSearch(event) {
   if (event.target.value.trim() === '') {
+    loadMoreBtnRef.disabled = true;
     clearMarkup();
     return;
   }
-  //   console.log(event.target.value);
   clearMarkup();
-  return fetchImage(event).then(renderMarkup).catch(error => console.log(error))
-  // .then(data => renderMarkup(data.hits));
+  queryStringParam.resetPage();
+  return fetchImage(event)
+    .then(renderMarkup)
+    .catch(error => console.log(error));
+}
+
+async function onBtnClick() {
+  queryStringParam.pageIncrement();
+  await fetchImageOnBtnClick()
+    .then(renderMarkup)
+    .catch(error => console.log(error));
+
+  scroll();
+  
 }
 
 function fetchImage(event) {
+  loadMoreBtnRef.disabled = false;
   return fetch(
     `${queryStringParam.LINK}${event.target.value}&page=${queryStringParam.page}&per_page=12&key=${queryStringParam.KEY}`,
+  )
+    .then(data => data.json())
+    .then(data => data.hits);
+}
+
+function fetchImageOnBtnClick() {
+  return fetch(
+    `${queryStringParam.LINK}${searchFormRef.value}&page=${queryStringParam.page}&per_page=12&key=${queryStringParam.KEY}`,
   )
     .then(data => data.json())
     .then(data => data.hits);
@@ -40,4 +73,17 @@ function renderMarkup(markup) {
 
 function clearMarkup() {
   galleryRef.innerHTML = '';
+}
+
+function scroll(index) {
+  const images = document.querySelectorAll('li');
+  const newImageIndex = document.querySelectorAll('li').length;
+  const element = images[newImageIndex-11];
+  // console.log(images);
+  // images.forEach(image => console.log(image))
+  // console.log(element);
+  element.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
 }
